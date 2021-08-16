2bdf94918edfcfd5179579b69cc4e8890b84c592
neel05 <neelrana6@gmail.com>
2021-08-16 21:11:38 +0530

    Case 1 EmptyString

--- src/TestCalculator.java
+++ src/TestCalculator.java
@@ -2,5 +2,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 public class TestCalculator {
-    
+    Calculator c = new Calculator();
+    @Test
+    public void TestEmptyString(){
+        assertEquals(0,c.Add(""));
+    }
 }
